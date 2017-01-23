68b4b707570342c1a13bf23336933a0771b04e0a
Amber <a0933095730@gmail.com>
2017-01-23 18:28:20 +0800

    first commit

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+$('.toggle-menu').on('click',function(){
+    if($(this).hasClass('active')){
+        $(this).removeClass('active');
+        $('#menu').fadeOut();
+    }
+    else{
+        $(this).addClass('active');
+        $('#menu').fadeIn();
+    }
+});
